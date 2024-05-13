24eae40784f862eec56647ba1c9ffbe390460d47
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-13 15:25:32 +0200

    :dizzy: Add logo fade out animation

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,18 +4,20 @@ import '../scss/Logo.scss'
 function Logo() {
     let delay = 250;
     let animation = 1000;
+    let freeze = 1500;
 
     setTimeout(() => document.querySelector('.line').classList.add('active'), delay);
     setTimeout(() => {
         document.querySelector('.major').classList.add('active');
         document.querySelector('.minor').classList.add('active');
     }, delay + animation);
+    setTimeout(() => document.querySelector('.container-logo').classList.add('disappeared'), delay + animation + freeze);
 
     return (
-        <div class="container-logo">
-            <div class="minor">&lt;</div>
-            <div class="major">&gt;</div>
-            <div class="line" />
+        <div className="container-logo">
+            <div className="minor">&lt;</div>
+            <div className="major">&gt;</div>
+            <div className="line" />
         </div>
     );
 }
