961ba8af5a2ed5a7802855d70d91ad64e8809ce7
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-15 16:17:05 +0200

    :adhesive_bandage: Use separated vars for LogoAnimated timing

--- src/components/LogoAnimated.jsx
+++ src/components/LogoAnimated.jsx
@@ -4,20 +4,22 @@ import '../styles/LogoAnimated.scss'
 import Logo from "./Logo";
 
 let delay = 250;
-let animation = 1000;
-let freeze = 1500;
+let lineAnimation = 1000;
+let symbolsFadeIn = 1000;
+let logoFreeze = 500;
+let total = delay + lineAnimation + symbolsFadeIn + logoFreeze;
 
 function LogoAnimated() {
     setTimeout(() => document.querySelector('.line').classList.add('active'), delay);
     setTimeout(() => {
         document.querySelector('.major').classList.add('active');
         document.querySelector('.minor').classList.add('active');
-    }, delay + animation);
-    setTimeout(() => document.querySelector('.container-logo').classList.add('disappeared'), delay + animation + freeze);
+    }, delay + lineAnimation);
+    setTimeout(() => document.querySelector('.container-logo').classList.add('disappeared'), total);
 
     return (<Logo />);
 }
 
-export const animationTime = delay + animation + freeze;
+export const animationTime = total;
 
 export default LogoAnimated;
