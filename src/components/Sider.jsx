764aec5534809f2895f0a10ed958ef4d8f39a94a
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-24 14:53:44 +0200

    :lipstick: Add socials icons

--- src/components/Sider.jsx
+++ src/components/Sider.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import '../styles/Sider.scss'
 
-function Sider({ orientation, content }) {
+function Sider({ orientation, content, children }) {
     return (
         <div className={`sider ${orientation}`}>
             <div className={`sider-content-${content}`}>
-
+                {children}
             </div>
             <div className='line' />
         </div>
