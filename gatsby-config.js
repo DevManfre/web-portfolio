3e4542015932b6db6b08b420e19910ff0121a546
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-24 15:23:53 +0200

    :wrench: remove gatsby extension

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,20 +3,15 @@
  */
 module.exports = {
     siteMetadata: {
-        title: `Alessio Manfredini`,
+        title: `devmanfre`,
         description: 'Alessio Manfredini web portfolio'
+
     },
     plugins: [
         {
             resolve: "gatsby-plugin-sass",
             options: {
                 additionalData: "@use 'Colors' as *;"
-            }
-        },
-        {
-            resolve: "gatsby-plugin-html-attributes",
-            options: {
-                lang: 'en'
             }
         }
     ]
