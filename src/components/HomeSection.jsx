173ae08469a3fee4f5b4c219fb58701570b8b18d
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-27 13:02:53 +0200

    :lipstick: Add right font-size

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -4,10 +4,10 @@ import '../styles/HomeSection.scss'
 function HomeSection() {
     return (
         <section className='home'>
-            <h1 className="first-text">Hi, my name is</h1>
+            <h1 className="top-subtitle">Hi, my name is</h1>
             <h2 className="name big-text">Alessio Manfredini</h2>
-            <h3 className="name-extend big-text">I build things for the web.</h3>
-            <p className="subtitle">I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
+            <h3 className="title big-text">I build things for the web.</h3>
+            <p className="bottom-subtitle">I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
         </section>
     );
 }
