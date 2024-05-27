6322413e3b05f2323e7105887c192e57f103ae77
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-27 16:57:58 +0200

    :dizzy: Add translation delay for main texts

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,23 +1,23 @@
-import * as React from 'react'
+import * as React from 'react';
 import { useInView } from 'framer-motion';
-import '../styles/HomeSection.scss'
+import '../styles/HomeSection.scss';
 
 function HomeSection() {
     const ref = React.useRef(null);
     const isInView = useInView(ref, { once: true });
     let inViewStyle = {
-        transform: isInView ? "none" : "translateY(100px)",
+        transform: isInView ? "none" : "translateY(50px)",
         opacity: isInView ? 1 : 0,
         transition: '1s'
     };
 
     return (
-        <section className='home' ref={ref}>
-            <div className="main-text" style={inViewStyle}>
-                <h1 className="top-subtitle">Hi, my name is</h1>
-                <h2 className="name big-text">Alessio Manfredini</h2>
-                <h3 className="title big-text">I build things for the web.</h3>
-                <p className="bottom-subtitle">I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
+        <section className='home'>
+            <div className="main-text" ref={ref}>
+                <h1 className="top-subtitle" style={inViewStyle}>Hi, my name is</h1>
+                <h2 className="name big-text" style={{...inViewStyle, transitionDelay: '0.3s'}}>Alessio Manfredini</h2>
+                <h3 className="title big-text" style={{...inViewStyle, transitionDelay: '0.6s'}}>I build things for the web.</h3>
+                <p className="bottom-subtitle" style={{...inViewStyle, transitionDelay: '0.9s'}}>I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
             </div>
         </section>
     );
