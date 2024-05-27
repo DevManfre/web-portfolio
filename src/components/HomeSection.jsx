f2c21218a656ed97132fa49c189a752b3311bf9e
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-27 11:43:04 +0200

    :recycle: Refactored home section code

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -5,8 +5,8 @@ function HomeSection() {
     return (
         <section className='home'>
             <h1 className="first-text">Hi, my name is</h1>
-            <h2 className="name">Alessio Manfredini</h2>
-            <h3 className="name-extend">I build things for the web.</h3>
+            <h2 className="name big-text">Alessio Manfredini</h2>
+            <h3 className="name-extend big-text">I build things for the web.</h3>
             <p className="subtitle">I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
         </section>
     );
