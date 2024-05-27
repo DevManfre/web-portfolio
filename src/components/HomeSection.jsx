65e7d88c3d717fecd71e50d22e457823fa56e291
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-27 14:30:50 +0200

    :lipstick: Move higher the main text

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -4,10 +4,12 @@ import '../styles/HomeSection.scss'
 function HomeSection() {
     return (
         <section className='home'>
-            <h1 className="top-subtitle">Hi, my name is</h1>
-            <h2 className="name big-text">Alessio Manfredini</h2>
-            <h3 className="title big-text">I build things for the web.</h3>
-            <p className="bottom-subtitle">I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
+            <div className="main-text">
+                <h1 className="top-subtitle">Hi, my name is</h1>
+                <h2 className="name big-text">Alessio Manfredini</h2>
+                <h3 className="title big-text">I build things for the web.</h3>
+                <p className="bottom-subtitle">I’m a software engineer specializing in building (and occasionally designing) web experiences.</p>
+            </div>
         </section>
     );
 }
