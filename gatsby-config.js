65cb16166d249c6f3498d7de74daa0f3e6870338
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-29 11:38:41 +0200

    :wrench: Add plugins to gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,40 @@ module.exports = {
             options: {
                 additionalData: "@use 'Colors' as *;"
             }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/locales`,
+                name: `locale`
+            }
+        },
+        {
+            resolve: `gatsby-plugin-react-i18next`,
+            options: {
+                localeJsonSourceName: `locale`,
+                languages: [`en`, `it`],
+                defaultLanguage: `en`,
+                siteUrl: `http://localhost:8000/`,
+                i18nextOptions: {
+                    interpolation: {
+                        escapeValue: false
+                    },
+                    keySeparator: false,
+                    nsSeparator: false
+                },
+                /* pages: [
+                    {
+                        matchPath: '/:lang?/blog/:uid',
+                        getLanguageFromPath: true,
+                        excludeLanguages: ['es']
+                    },
+                    {
+                        matchPath: '/preview',
+                        languages: ['en']
+                    }
+                ] */
+            }
         }
     ]
 };
