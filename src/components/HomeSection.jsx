7c7bba406b98afafb61baa63e80fa643e64dd2a1
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-29 12:57:27 +0200

    :technologist: Add dev name from siteMetadata

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import { useInView } from 'framer-motion';
 import { navbarTotalFadeInTime } from './NavBar';
 import '../styles/HomeSection.scss';
@@ -10,12 +11,21 @@ const transitionDelay = 0.2;
 function HomeSection() {
     const ref = React.useRef(null);
     const isInView = useInView(ref, { once: true });
+    const lastNodeFadeInTime = navbarTotalFadeInTime + transitionDelay * 3;
+    const query = (useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata{
+                    developer
+                }
+            }
+        }
+    `)).site.siteMetadata;
     let inViewStyle = {
         transform: isInView ? "none" : "translateY(50px)",
         opacity: isInView ? 1 : 0,
         transition: `${transition}s`
     };
-    const lastNodeFadeInTime = navbarTotalFadeInTime + transitionDelay * 3;
 
     if (isInView) setTimeout(() => {
         for (let child of document.querySelector('.main-text').children) {
@@ -30,7 +40,9 @@ function HomeSection() {
                 <h1 className="top-subtitle" style={{ ...inViewStyle, transitionDelay: `${navbarTotalFadeInTime + transitionDelay * 0}s` }}>
                     <Trans>homepage-greeting</Trans>
                 </h1>
-                <h2 className="name big-text" style={{ ...inViewStyle, transitionDelay: `${navbarTotalFadeInTime + transitionDelay * 1}s` }}>Alessio Manfredini</h2>
+                <h2 className="name big-text" style={{ ...inViewStyle, transitionDelay: `${navbarTotalFadeInTime + transitionDelay * 1}s` }}>
+                    {query.developer}
+                </h2>
                 <h3 className="title big-text" style={{ ...inViewStyle, transitionDelay: `${navbarTotalFadeInTime + transitionDelay * 2}s` }}>
                     <Trans>homepage-subtitle</Trans>
                 </h3>
