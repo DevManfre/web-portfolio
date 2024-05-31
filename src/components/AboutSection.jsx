31f626fe22ee6ad408eb9290eb8c0c0d2c73eb29
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-05-31 18:13:45 +0200

    :construction: Work in progress for AboutSection

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { useInView } from 'framer-motion';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Trans } from 'react-i18next';
+import '../styles/AboutSection.scss';
+
+function HomeSection() {
+    const ref = React.useRef(null);
+    const isInView = useInView(ref, { once: true });
+    let inViewStyle = {
+        transform: isInView ? "none" : "translateY(50px)",
+        opacity: isInView ? 1 : 0,
+        transition: `1s`
+    };
+
+    return (
+        <section id='about' >
+            <div className="main-text" ref={ref} style={{ ...inViewStyle }}>
+                <h2 className='section-title'>About Me</h2>
+                <div className='inner'>
+                    <div className='text'>
+                        <p><Trans>about-paragraph-1</Trans></p>
+                        <p><Trans>about-paragraph-2</Trans></p>
+                        <p><Trans>about-paragraph-3</Trans></p>
+                    </div>
+                    <div className='img'>
+                        <StaticImage alt='' src='../images/profile.webp' />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default HomeSection;
