23e9735653eb2cd4ad021897b6429f8351261032
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-06-14 15:40:06 +0200

    :bulb: Add comments for css handler external library

--- src/utils/cssModuleUtils.js
+++ src/utils/cssModuleUtils.js
@@ -1,13 +1,17 @@
 import { useInView } from 'framer-motion';
 
+/* This function covert a css transition value to a number (without the final s unit)  */
 export const fromCssSecondsToNumber = (cssSeconds) => {
     return cssSeconds.replace('s', '');
 }
 
+/* This function covert a css transition value to a number (without the final s unit).
+Then it's converted to milliseconds */
 export const fromCssSecondsToJsMilliseconds = (cssSeconds) => {
     return fromCssSecondsToNumber(cssSeconds) * 1000;
 }
 
+/* This function return a global isInView var for all react site. */
 export const useIsInView = (reference) => {
     return useInView(
         reference,
@@ -17,6 +21,7 @@ export const useIsInView = (reference) => {
     );
 }
 
+/* This function return a global style for all components entering in the viewport. */
 export const getInViewStyle = (isInView, transition = 1, translateY = 50) => {
     return {
         opacity: isInView ? 1 : 0,
