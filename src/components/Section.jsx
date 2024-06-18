b99be050a2488e6df1df5a02639d671bfbc71696
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-06-18 11:46:21 +0200

    :recycle: Refactored about section

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,9 +1,9 @@
 import * as React from "react";
 import {sectionTitle} from '../styles/Section.module.scss';
 
-function Section({ children, id, reference, style, title }) {
+function Section({ children, classes, id, reference, style, title }) {
     return (
-        <section id={id}>
+        <section id={id} className={classes}>
             <div className="inner" ref={reference} style={style}>
                 <div className={sectionTitle}>
                     {title}
