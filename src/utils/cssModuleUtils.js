5d7304807eb17a68839a19875617fdb02fe9888c
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-06-20 10:25:17 +0200

    :recycle: Minify css module library

--- src/utils/cssModuleUtils.js
+++ src/utils/cssModuleUtils.js
@@ -1,26 +1,14 @@
 import { useInView } from 'framer-motion';
 
 /* This function covert a css transition value to a number (without the final s unit)  */
-export const fromCssSecondsToNumber = (cssSeconds) => {
-    return cssSeconds.replace('s', '');
-}
+export const fromCssSecondsToNumber = cssSeconds => cssSeconds.replace('s', '');
 
 /* This function covert a css transition value to a number (without the final s unit).
 Then it's converted to milliseconds */
-export const fromCssSecondsToJsMilliseconds = (cssSeconds) => {
-    return fromCssSecondsToNumber(cssSeconds) * 1000;
-}
+export const fromCssSecondsToJsMilliseconds = cssSeconds => fromCssSecondsToNumber(cssSeconds) * 1000;
 
 /* This function return a global isInView var for all react site. */
-export const useIsInView = (reference) => {
-    return useInView(
-        reference,
-        {
-            once: true,
-            amount: 0.3
-        }
-    );
-}
+export const useIsInView = reference => useInView(reference, { once: true, amount: 0.3 });
 
 /* This function return a global style for all components entering in the viewport. */
 export const getInViewStyle = (isInView, transition = '1', translateY = 50) => {
@@ -31,6 +19,5 @@ export const getInViewStyle = (isInView, transition = '1', translateY = 50) => {
     };
 }
 
-export const fromPxToInt = (px) => {
-    return px.replace('px', '');
-}
+/* This function convert css px value into int */
+export const fromPxToInt = px => px.replace('px', '');
