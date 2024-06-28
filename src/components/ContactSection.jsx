153a3e9ee048505b0693605020a2009adbaf5f9e
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-06-28 12:20:51 +0200

    :construction: Working on contact component

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
+import { Trans } from "react-i18next";
+import { graphql, useStaticQuery } from 'gatsby';
 import Section from '../components/Section';
-import {contact} from '../styles/ContactSection.module.scss';
+import { icons } from '../utils/svgIcons';
+import { contact } from '../styles/ContactSection.module.scss';
 
 function ContactSection() {
+    const email = (useStaticQuery(graphql`query{site{siteMetadata{email}}}`)).site.siteMetadata.email;
+
     return (
         <Section id='contact' classes={contact} central='true' title='Contact'>
-            
+            <p>
+                <Trans>contact-message</Trans>
+
+                <div>
+                    <a href={`mailto:${email}`} title='Email'>
+                        {icons['email']}
+                        <Trans>contact-email-btn</Trans>
+                    </a>
+                </div>
+            </p>
         </Section>
     );
 }
