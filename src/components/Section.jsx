0fbad51e7f783841d10f5c2fda1418d91f39647d
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-07-01 09:39:12 +0200

    :lipstick: Add subtitle in centered sections

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { Trans } from "react-i18next";
 import { getInViewStyle, useIsInView } from '../utils/cssModuleUtils';
-import { sectionTitle } from '../styles/Section.module.scss';
+import { sectionTitle, centralSectionSubtitle } from '../styles/Section.module.scss';
 
 function Section({ central, children, classes, id, reference, style, title }) {
     const defaultRef = React.useRef(null);
@@ -9,6 +10,11 @@ function Section({ central, children, classes, id, reference, style, title }) {
     return (
         <section id={id} className={classes}>
             <div className="inner" ref={reference ? reference : defaultRef} style={reference ? style : defaultInViewStyle}>
+                {central == 'true' &&
+                    <div className={centralSectionSubtitle}>
+                        <Trans>section-subtitle</Trans>
+                    </div>
+                }
                 <div className={sectionTitle} central={central ? central : 'false'}>
                     {title}
                 </div>
