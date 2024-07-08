e8164ae8124bfabc2673ba847f5fe0df85e57dc5
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2024-07-08 14:11:33 +0200

    :lipstick: Set new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -154,6 +154,12 @@ module.exports = {
                 }
             }
         },
+        {
+            resolve: 'gatsby-plugin-manifest',
+            options: {
+                "icon": "src/images/icon.png"
+            }
+        },
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`
     ]
