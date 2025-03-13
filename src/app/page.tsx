899c74d02448bfc4cae66a321519d71083c085b5
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2025-03-13 15:35:39 +0100

    🔧 Add button components for LinkedIn and email contact options in the contact section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { ProjectCard } from "@/components/project-card";
 import { ResumeCard } from "@/components/resume-card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { DATA } from "@/data/resume";
 
 const BLUR_FADE_DELAY = 0.04;
@@ -123,7 +124,7 @@ export default function Page() {
                     </div>
                 </div>
             </section>
-            <section id="projects">
+            {/* <section id="projects">
                 <div className="space-y-12 w-full py-12">
                     <BlurFade delay={BLUR_FADE_DELAY * 11}>
                         <div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -154,39 +155,7 @@ export default function Page() {
                         ))}
                     </div>
                 </div>
-            </section>
-            <section id="hackathons">
-                <div className="space-y-12 w-full py-12">
-                    <BlurFade delay={BLUR_FADE_DELAY * 13}>
-                        <div className="flex flex-col items-center justify-center space-y-4 text-center">
-                            <div className="space-y-2">
-                                <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">Hackathons</div>
-                                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">I like building things</h2>
-                                <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                                    During my time in university, I attended {DATA.hackathons.length}+ hackathons. People from around the country would come together and build incredible things in 2-3
-                                    days. It was eye-opening to see the endless possibilities brought to life by a group of motivated and passionate individuals.
-                                </p>
-                            </div>
-                        </div>
-                    </BlurFade>
-                    <BlurFade delay={BLUR_FADE_DELAY * 14}>
-                        <ul className="mb-4 ml-4 divide-y divide-dashed border-l">
-                            {DATA.hackathons.map((project, id) => (
-                                <BlurFade key={project.title + project.dates} delay={BLUR_FADE_DELAY * 15 + id * 0.05}>
-                                    <HackathonCard
-                                        title={project.title}
-                                        description={project.description}
-                                        location={project.location}
-                                        dates={project.dates}
-                                        image={project.image}
-                                        links={project.links}
-                                    />
-                                </BlurFade>
-                            ))}
-                        </ul>
-                    </BlurFade>
-                </div>
-            </section>
+            </section> */}
             <section id="contact">
                 <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
                     <BlurFade delay={BLUR_FADE_DELAY * 16}>
@@ -194,7 +163,19 @@ export default function Page() {
                             <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">Contact</div>
                             <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Get in Touch</h2>
                             <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                                Want to chat? Just shoot me a dm and I&apos;ll respond whenever I can. I will ignore all soliciting.
+                                Want to chat? Just shoot me a&nbsp;
+                                <a href={DATA.contact.social.LinkedIn.url} target="about:blank">
+                                    <Button variant="link" className="p-0 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                        dm
+                                    </Button>
+                                </a>
+                                &nbsp;on Linkedin or send me an&nbsp;
+                                <Button variant="link" className="p-0 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                                    <a href={DATA.contact.social.email.url} target="about:blank">
+                                        email
+                                    </a>{" "}
+                                </Button>
+                                &nbsp;and I&apos;ll respond whenever I can.
                             </p>
                         </div>
                     </BlurFade>
