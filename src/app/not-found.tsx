db4b7d730ad856a53a50e876b9d766e4ebf45d0e
Alessio Manfredini <alessio.manfredini.work@gmail.com>
2025-04-18 13:01:10 +0200

    💫 Enhance 404 Not Found page with BlurFade animations for improved visual effects

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,24 +4,34 @@ import GlitchText from "@/components/reactbits/TextAnimations/GlitchText/GlitchT
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
+import BlurFade from "@/components/magicui/blur-fade";
 
 export default function NotFound() {
     const speed = 1.3,
         enableShadows = false,
-        t = useTranslations();
+        t = useTranslations(),
+        delay = 0.4,
+        offset = 10,
+        blur = "4px";
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <GlitchText speed={speed} enableShadows={enableShadows}>
-                404
-            </GlitchText>
-            <GlitchText speed={speed} enableShadows={enableShadows}>
-                {t("PageNotFound.page-not-found")}
-            </GlitchText>
+            <BlurFade duration={speed} delay={delay} yOffset={offset} blur={blur}>
+                <GlitchText speed={speed} enableShadows={enableShadows}>
+                    404
+                </GlitchText>
+            </BlurFade>
+            <BlurFade duration={speed} delay={delay * 2} yOffset={offset} blur={blur}>
+                <GlitchText speed={speed} enableShadows={enableShadows}>
+                    {t("PageNotFound.page-not-found")}
+                </GlitchText>
+            </BlurFade>
 
-            <Link href="/" className="mt-12">
-                <Button>{t("PageNotFound.back-to-site")}</Button>
-            </Link>
+            <BlurFade duration={speed} delay={delay * 3} yOffset={offset} blur={blur} className="mt-12">
+                <Link href="/">
+                    <Button>{t("PageNotFound.back-to-site")}</Button>
+                </Link>
+            </BlurFade>
         </div>
     );
 }
